9b2f5db8cf7f9919f7ba6be252fb55226537bb1c
JackT1009 <111690363+JackT1009@users.noreply.github.com>
2025-03-01 00:39:18 +0000

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,7 @@
+-- Paste into /config.cfg
+{
+    debug_mode = true,
+    export_direction = "down",
+    alert_player = "@YourUsername",
+    emergency_items = ["minecraft:bread", "minecraft:torch"]
+}
