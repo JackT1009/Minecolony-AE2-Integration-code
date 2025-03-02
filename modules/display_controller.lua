bc0a0c1f4ba9b1ceaf0de678cd1aa2b264b8e718
JackT1009 <111690363+JackT1009@users.noreply.github.com>
2025-03-02 17:51:30 +0000

    Update display_controller.lua

--- modules/display_controller.lua
+++ modules/display_controller.lua
@@ -1,59 +1,62 @@
 local config = require("modules.config")
 
 local DisplayController = {
-    last_lines = {},
+    last_content = {},
     page = 1
 }
 
-function DisplayController.initialize(monitor)
-    DisplayController.mon = monitor
-    if monitor then
-        monitor.setTextScale(0.5)
-        monitor.setBackgroundColor(colors.black)
-        monitor.clear()
-    end
+function DisplayController.initialize(mon)
+    DisplayController.mon = mon
+    if not mon then return end
+    
+    mon.setTextScale(0.5)
+    mon.setBackgroundColor(colors.black)
+    mon.clear()
+    mon.setCursorPos(1,1)
 end
 
 function DisplayController.update(statuses, debug_info, time_left)
     if not DisplayController.mon then return end
+    local mon = DisplayController.mon
+    local w, h = mon.getSize()
     
-    local w, h = DisplayController.mon.getSize()
-    local lines = {}
+    -- Clear screen properly
+    mon.setBackgroundColor(colors.black)
+    mon.clear()
 
-    -- Header with timer (blue)
-    DisplayController.mon.setTextColor(colors.blue)
-    DisplayController.mon.setCursorPos(1, 1)
-    DisplayController.mon.write(("Colony Manager [Pg %d] | Refresh: %ds "):format(
-        DisplayController.page,
-        math.floor(time_left)
-    ))
+    -- Header
+    mon.setTextColor(colors.blue)
+    mon.setCursorPos(1,1)
+    mon.write(("Colony Manager v2.1 | Refresh: %ds "):format(math.floor(time_left)))
 
-    -- Status items (color-coded)
-    local line_num = 3
-    local start_idx = (DisplayController.page - 1) * config.MAX_ITEMS_DISPLAY + 1
-    for i = start_idx, math.min(start_idx + config.MAX_ITEMS_DISPLAY - 1, #statuses) do
+    -- Status lines
+    local line = 3
+    for i = 1, math.min(#statuses, config.MAX_ITEMS_DISPLAY) do
         local s = statuses[i]
-        local color = colors.white
-        if s.status == "/" then color = colors.green
-        elseif s.status == "M" then color = colors.yellow
-        else color = colors.red end
+        mon.setCursorPos(1, line)
+        
+        if s.status == "/" then
+            mon.setTextColor(colors.green)
+        elseif s.status == "M" then
+            mon.setTextColor(colors.yellow)
+        else
+            mon.setTextColor(colors.red)
+        end
 
-        DisplayController.mon.setTextColor(color)
-        DisplayController.mon.setCursorPos(1, line_num)
-        DisplayController.mon.write(("%-18s %s %3d/%3d"):format(
-            s.name,
+        mon.write(("%-18s %s %3d/%3d"):format(
+            s.name:sub(1,18),
             s.status,
             s.available,
             s.needed
         ))
-        line_num = line_num + 1
+        line = line + 1
     end
 
-    -- Debug footer (white)
-    DisplayController.mon.setTextColor(colors.white)
-    for i = 1, config.DEBUG_MAX_LINES do
-        DisplayController.mon.setCursorPos(1, h - config.DEBUG_MAX_LINES + i - 1)
-        DisplayController.mon.write(debug_info[i] or "")
+    -- Debug footer
+    mon.setTextColor(colors.lightGray)
+    for i = 1, math.min(#debug_info, config.DEBUG_MAX_LINES) do
+        mon.setCursorPos(1, h - config.DEBUG_MAX_LINES + i - 1)
+        mon.write(debug_info[i]:sub(1,w))
     end
 end
 
