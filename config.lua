afdd24d65d21093580b0aaad4a1271d83e96cec7
JackT1009 <111690363+JackT1009@users.noreply.github.com>
2025-03-02 00:54:17 +0000

    Delete config.lua

--- config.lua
+++
@@ -1,7 +0,0 @@
--- Paste into /config.cfg
-{
-    debug_mode = true,
-    export_direction = "down",
-    alert_player = "@YourUsername",
-    emergency_items = ["minecraft:bread", "minecraft:torch"]
-}
