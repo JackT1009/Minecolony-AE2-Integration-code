5616aacb359783194c46165688289b12434471ba
JackT1009 <111690363+JackT1009@users.noreply.github.com>
2025-03-02 00:01:32 +0000

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -5,19 +5,17 @@ local mon = peripheral.find("monitor") or peripheral.wrap("right")
 
 -- Load Modules
 local Colony = require("modules/colony")
-local Display = require("modules/display")
 local InventoryChecker = require("modules/inventory_checker")
+local Display = require("modules/display")  -- Load LAST
 
 -- Initialize Systems
 local inventoryCheck = InventoryChecker.new(bridge)
-Display.mon = mon  -- Critical fix here
+Display.mon = mon  -- Simple assignment
 
 -- Main Loop
 while true do
     local requests = Colony.getRequests()
     local statuses = inventoryCheck:getAllStatuses(requests)
-    
     Display.showStatuses(statuses)
-    
     sleep(5)
 end
