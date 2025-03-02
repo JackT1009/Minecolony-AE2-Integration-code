54b070a9075f2aaf89162d8627a101a2985ca857
JackT1009 <111690363+JackT1009@users.noreply.github.com>
2025-03-02 00:37:08 +0000

    Update colony.lua

--- modules/colony.lua
+++ modules/colony.lua
@@ -1,27 +1,20 @@
 local Colony = {}
-Colony.__index = Colony
 
 function Colony.getRequests()
     local integrator = peripheral.find("colonyIntegrator")
     if not integrator then return {} end
     
-    local rawRequests = integrator.getRequests() or {}
-    local processed = {}
-    
-    for _, req in pairs(rawRequests) do
+    local requests = {}
+    for _, req in pairs(integrator.getRequests() or {}) do
         if req.items and req.items[1] then
-            -- Clean item names
-            local rawName = req.items[1].name
-            local cleanName = string.lower(string.match(rawName, "([^:]+:[^:]+)"))
-            
-            table.insert(processed, {
+            local cleanName = req.items[1].name:lower():gsub(":[^:]+$", "")
+            table.insert(requests, {
                 name = cleanName,
                 count = req.count
             })
         end
     end
-    
-    return processed
+    return requests
 end
 
 return Colony
