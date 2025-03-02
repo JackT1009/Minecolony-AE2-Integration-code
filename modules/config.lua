fbd9fc1977ef4437dccf320989b89d8b679cdc0d
JackT1009 <111690363+JackT1009@users.noreply.github.com>
2025-03-02 18:03:52 +0000

    Update config.lua

--- modules/config.lua
+++ modules/config.lua
@@ -1,7 +1,7 @@
 return {
     WAREHOUSE_DIRECTION = "up",
-    REFRESH_INTERVAL = 5,  -- Test with 5s for debugging
-    CACHE_TIME = 2,
+    REFRESH_TICKS = 20 * 5,  -- 5 seconds (20 ticks/sec)
+    CACHE_TICKS = 20 * 2,    -- 2 seconds
     DEBUG_MAX_LINES = 3,
     MAX_ITEMS_DISPLAY = 12
 }
