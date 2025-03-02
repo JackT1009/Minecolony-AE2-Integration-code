41f464781601f5cb93d629890ab1b8c80370e908
JackT1009 <111690363+JackT1009@users.noreply.github.com>
2025-03-02 17:33:00 +0000

    Update config.lua

--- modules/config.lua
+++ modules/config.lua
@@ -1,7 +1,8 @@
+-- modules/config.lua
 return {
     WAREHOUSE_DIRECTION = "up",
-    REFRESH_INTERVAL = 7,  -- Optimal balance for AE2
-    CACHE_TIME = 3,        -- Seconds to cache ME data
+    REFRESH_INTERVAL = 7,
+    CACHE_TIME = 3,
     DEBUG_MAX_LINES = 5,
-    MAX_ITEMS_DISPLAY = 15 -- Per screen page
+    MAX_ITEMS_DISPLAY = 15
 }
