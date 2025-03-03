c854bffebc18cd11dfb5ace82b6e9558414dd5b3
JackT1009 <111690363+JackT1009@users.noreply.github.com>
2025-03-03 20:31:19 +0000

    Update colony.lua

--- modules/colony.lua
+++ modules/colony.lua
@@ -1,11 +1,17 @@
-local Colony = {}
+-- colony.lua
+local Colony = {
+    integrator = nil
+}
+
+function Colony.initialize(integrator)
+    Colony.integrator = integrator
+end
 
 function Colony.getRequests()
-    local integrator = peripheral.find("colonyIntegrator")
-    if not integrator then return {} end
+    if not Colony.integrator then return {} end
     
     local requests = {}
-    for _, req in pairs(integrator.getRequests() or {}) do
+    for _, req in pairs(Colony.integrator.getRequests() or {}) do
         if req.items and req.items[1] then
             requests[#requests+1] = {
                 name = req.items[1].name,
